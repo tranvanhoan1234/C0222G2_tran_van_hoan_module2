f70700005354181ed0e42a9c17f61bbe7ef1112f
tranvanhoan1234 <89346268+tranvanhoan1234@users.noreply.github.com>
2022-04-17 22:01:36 +0700

    up

--- src/ss11_stack_queue/reverse_stack.java
+++ src/ss11_stack_queue/reverse_stack.java
@@ -4,18 +4,30 @@ import java.util.Stack;
 
 public class reverse_stack {
     public static void main(String[] args) {
-        Stack<Integer> stack=new Stack<>();
-        stack.push (8);
-        stack.push (9);
-        stack.push (10);
-        stack.peek();
 
-        System.out.println(stack.peek(
+//        Scanner scanner = new Scanner(System.in);
+        Stack<Integer> stack = new Stack<>();
+//        System.out.println("nhập CÁI L GÌ BẠN THÍCH bạn ây");
+//        String s = scanner.nextLine();
+//        for (int i = 0; i < s.length(); i++) {
+//            stack.push(s.charAt(i) + "");
+//        }
+//        System.out.println("CHUỖI ĐẢO NGƯỢC");
+//        for (int i = 0; i < s.length(); i++) {
+//            System.out.printf(stack.pop());
 
-
-        ));
+stack.push(1);
+stack.push(2);
+stack.push(3);
+stack.push(4);
+stack.push(5);
         System.out.println(stack);
+        for (int i = 0; i < stack.size(); ) {
+            System.out.println(stack.pop());
 
+        }
+        }
     }
 
-}
+
+
